889b29837e044023fe84b69121f01076729c331c
denchdudarev <dudarev94@gmail.com>
2022-03-04 09:40:13 +0300

    02

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -5,7 +5,7 @@ function Profile() {
     return (
         <div className="b-content">
             <div className="b-profile">
-                <img src="https://static.dw.com/image/57337665_403.jpg" alt="" />
+                <img src="https://panoramy-chernobylya.ru/wp-content/uploads/2018/11/CHem-otlichaetsya-CHernobyl-ot-Pripyati-1-1024x576.jpg" alt="" />
                 <ProfileInfo />
             </div>
         </div>
